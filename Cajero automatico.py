57adf5fd36769dc4ca6b63ada7da89533ac8677a
AndrxsGutierrez <andresgutierrezospino@gmail.com>
2026-03-04 20:51:35 -0500

    Add files via upload

--- Cajero automatico.py
+++ Cajero automatico.py
@@ -1,7 +1,3 @@
-Inicial_Balance = 1000
-Pin = 2478
-contador = 0
-
 def operations():
     try:
         quantity = int(input("Cuantas operaciones desea realizar: "))
@@ -53,55 +49,89 @@ def DepMoney():
             print ('No puedes ingresar letras, intente nuevamente')
             return DepMoney() 
 
-while co
-    pin = int(input('Ingrese su pin para poder acceder al menu: '))
-    if pin == Pin:
-        print ('Acceso permitido')
-
-    if pin != Pin:
-        print ('haz errado, te quedan 3 intentos')
-        if pin != Pin:
-            print ('haz errado, te quedan 2 intentos')
-            if pin != Pin:
-                print ('haz errado, te quedan 1 intentos')
-                if pin != Pin:
-                    print ('haz errado, te quedan 0 intentos')
-                    break
-        
-    
-
-    operation = operations()
-
-    for I in range (operation):
-
-        print('     RiwiBank      ')
-        print('-------MENU-------')
-        print('1 - Consultar saldo')
-        print('2 - Retirar dinero')
-        print('3 - Depositar dinero')
-        print('4 - Salir')
-
-        opcion_Num = opcion()
-        
-        if opcion_Num == 1:
-            print('\n','Su saldo actual es de',Inicial_Balance,'\n')
-
-        elif opcion_Num == 2:
-            montoactual = RetMoney()
-            Inicial_Balance -= montoactual
-            print ('\n','Usted ha retirado',montoactual)
-            print ('Su saldo actual es de',Inicial_Balance,'\n')
-
-        elif opcion_Num == 3:
-            montoactual = DepMoney()
-            Inicial_Balance += montoactual
-            print ('\n','Usted ha depositado',montoactual)
-            print ('Su saldo actual es de',Inicial_Balance,'\n')
-        
+def Userde():
+    try:
+        quantity = int(input('Ingresa tu numero de usuario: '))
+        if quantity <=0:
+            print("ingrese un numero mayor a 0")  
+            return operations()
         else:
-            break
+            return quantity
+    except ValueError:
+        print("Ingrese un numero.")
+        return operations()
+
+def Clavede():
+    try:
+        quantity = int(input('Ingresa tu clave: '))
+        if quantity <=0:
+            print("ingrese un numero mayor a 0")  
+            return operations()
+        else:
+            return quantity
+    except ValueError:
+        print("Ingrese un numero.")
+        return operations()
+
+
+Usuario = 1234
+clave = 2122
+Inicial_Balance = 1000
+contador = 0
+
+print ('\n','Solo tienes 4 intentos')
+while contador < 4:
     
-    print ('Gracias por usar nuestro cajero automatico')
+    Usuarioped = Userde()
+    claveoped  = Clavede()
+    if Usuarioped == Usuario and claveoped == clave:
+        print('\n','Usted ha ingresado correctamente.','\n')
+        break
+        
+    elif Usuarioped != Usuario or claveoped != clave:
+        print('\n','Acceso denegado, intente nuevamente.','\n')
+        contador += 1
+if contador == 4:
+    print('\n','Haz hecho demasiados intentos','\n')
+    exit()
+
+    
+
+operation = operations()
+
+for I in range (operation):
+
+    print('     RiwiBank      ')
+    print('-------MENU-------')
+    print('1 - Consultar saldo')
+    print('2 - Retirar dinero')
+    print('3 - Depositar dinero')
+    print('4 - Salir')
+    print('-------------------','\n')
+
+
+    opcion_Num = opcion()
+    montoactual = RetMoney()
+    
+    if opcion_Num == 1:
+        print('\n','Su saldo actual es de',Inicial_Balance,'\n')
+
+    elif opcion_Num == 2:
+        Inicial_Balance -= montoactual
+        print ('\n','Usted ha retirado',montoactual)
+        print ('Su saldo actual es de',Inicial_Balance,'\n')
+
+    elif opcion_Num == 3:
+        montoactual = DepMoney()
+        Inicial_Balance += montoactual
+        print ('\n','Usted ha depositado',montoactual)
+        print ('Su saldo actual es de',Inicial_Balance,'\n')
+    
+    else:
+        print(' ')
+        break
+
+print ('Gracias por usar nuestro cajero automatico!','\n')
 
     
     
